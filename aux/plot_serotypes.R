efc941ee2f7839b650f157538bd7d606a579a048
Max <lmcarvalho@ufrj.br>
2014-09-22 18:55:41 -0300

    new serotype plot

--- aux/plot_serotypes.R
+++ aux/plot_serotypes.R
@@ -1,3 +1,4 @@
 dt <- read.table("..//denv_serotypes_Rio.csv", sep = "\t", header = TRUE)
-matplot(dt, type = "l", lwd = 2, col = 1:3, lty =1)
+matplot(1993:2010, dt, type = "l", lwd = 2, col = 1:3,
+        lty = 1, xlab = "Time (years)", ylab = "Viral isolation")
 legend(x="topleft", legend = c("DENV-1", "DENV-2", "DENV-3"), col = 1:3, lty = 1, bty = "n")
