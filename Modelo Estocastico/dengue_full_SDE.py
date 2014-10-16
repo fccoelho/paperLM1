da33f4d01dafc125802e30a9dedf322644c9d79e
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-10-16 11:01:01 -0300

    fixed bugs in the model; it is still not parameterized correctly...

--- Modelo Estocastico/dengue_full_SDE.py
+++ Modelo Estocastico/dengue_full_SDE.py
@@ -1,5 +1,5 @@
 # coding:utf8
-
+from __future__ import division
 from BIP.SDE.gillespie import Model
 import time
 from numpy import array, genfromtxt
@@ -12,35 +12,38 @@ vnames = ['S', 'I1', 'I2', 'I3', 'I4', 'R1', 'R2', 'R3', 'R4', 'I11',
 # transitions
 # S->I1	S->I2	S->I3	S->I4	I1=>R1	I2=>R2	I3=>R3	I4=>R4	R1=>I11	R1=>I12	R1=>I13	R1=>I14	R2=>I21	R2=>I22	R2=>I23	R2=>I24	R3=>I31	R3=>I32	R3=>I33	R3=>I34	R4=>I41	R4=>I42	R4=>I43	R4=>I44	I11=>R	I12=R	I13=>R	I14=R	I21=>R	I22=R	I23=>R	I24=R	I31=>R	I32=R	I33=>R	I34=R	I41=>R	I42=R	I43=>R	I44=R
 
-tm = genfromtxt('tmat.csv', delimiter=',', skip_header=2, usecols=range(1, 67), dtype=int)
+tm = genfromtxt('tmat.csv', delimiter=',', skip_header=2, usecols=range(1, 68), dtype=int)
 
 # Initial Conditions
 # Initial conditions for each of the four Serotype model
 
-N = 1000
+N = 5000
 
-ini = [N - 40, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+ini = [N, 150, 150, 150, 150, 150, 150, 150, 150, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 pars = (1 / 70.,  # mu
         400,  # beta
         1,  # phi
         100,  # sigma
-        1)  # gamma
+        1.7)  # gamma
 
 # Propensity functions
 def gen_prop_functs():
-    nat_code = "def fnat(r, ini): return r[0]*N\n"
+    nat_code = "def fnat(r, ini): return r[0]*sum(ini)\n"
     mu_code = "def fmu{}(r, ini): return r[0]*ini[{}]\n"
-    inf_code = """def fS(r, ini): return ini[0]*sum([lamb(i) for i in range(4)])\n"""
+    # inf_code = """def fS(r, ini): return ini[0]*sum([lamb(i) for i in range(4)])\n"""
     inf2_code = """def f{}(r, ini): return ini[0]* lamb({})\n"""
-    rec_code = """def f{}(r, ini): return r[-2]*ini[{}]\n"""
-    inf3_code = """def f{}(r, ini): return r[-1]*ini[{}]*lamb({})\n"""
+    rec_code = """def f{}(r, ini): return r[3]*ini[{}]\n"""
+    inf3_code = """def f{}(r, ini): return r[4]*ini[{}]*lamb({})\n"""
 
     nt = tm.shape[1]
     with open('propfun.py', 'w') as f:
         f.write("pars = {}\nini = {}\nN = {}\n\n".format(pars, ini, N))
         f.write("lamb = lambda i: {}*(ini[i+1] + sum(ini[9+i : 9+12+i+1 : 4])-ini[9+4*i+1])\n".format(pars[1]))
-        f.write(inf_code)
+        # f.write(inf_code)
+
+        f.write("\n#Natality\n\n")
+        f.write(nat_code)
 
         f.write("\n#Infection rates\n\n")
         for i, n in enumerate(['S_I1', 'S_I2', 'S_I3', 'S_I4']):  # Infection rates
@@ -48,7 +51,7 @@ def gen_prop_functs():
 
         f.write("\n#Primary recovery rates\n\n")
         for i, n in enumerate(['I1_R1', 'I2_R2', 'I3_R3', 'I4_R4']):
-            f.write(rec_code.format(n, i + 5))
+            f.write(rec_code.format(n, i + 1))
 
         f.write("\n#Secondary Infection rates\n\n")
         for i, n in enumerate(
@@ -72,21 +75,37 @@ def gen_prop_functs():
 gen_prop_functs()
 from propfun import *
 
-propensity = [fS, fS_I1, fS_I2, fS_I3, fS_I4, fI1_R1, fI2_R2, fI3_R3, fI4_R4, fR1_I11, fR1_I12, fR1_I13, fR1_I14,
-              fR2_I21, fR2_I22, fR2_I23, fR2_I24, fR3_I31, fR3_I32, fR3_I33, fR3_I34, fR4_I41, fR4_I42, fR4_I43,
-              fR4_I44, fI11_R, fI12_R, fI13_R, fI14_R, fI21_R, fI22_R, fI23_R, fI24_R, fI31_R, fI32_R, fI33_R, fI34_R,
-              fI41_R, fI42_R, fI43_R, fI44_R, fmuS, fmuI1, fmuI2, fmuI3, fmuI4, fmuI11, fmuI12, fmuI13, fmuI14, fmuI21,
-              fmuI22, fmuI23, fmuI24, fmuI31, fmuI32, fmuI33, fmuI34, fmuI41, fmuI42, fmuI43, fmuI44, fmuR]
+propensity = [fnat,  # Natality
+              fS_I1, fS_I2, fS_I3, fS_I4,  # Primary infections
+              fI1_R1, fI2_R2, fI3_R3, fI4_R4,  # primary recovery
+              fR1_I11, fR1_I12, fR1_I13, fR1_I14,  #Secondary infections
+              fR2_I21, fR2_I22, fR2_I23, fR2_I24,  #Secondary infections
+              fR3_I31, fR3_I32, fR3_I33, fR3_I34,  #Secondary infections
+              fR4_I41, fR4_I42, fR4_I43, fR4_I44,  #Secondary infections
+              fI11_R, fI12_R, fI13_R, fI14_R,  # Secondary recovery
+              fI21_R, fI22_R, fI23_R, fI24_R,  # Secondary recovery
+              fI31_R, fI32_R, fI33_R, fI34_R,  # Secondary recovery
+              fI41_R, fI42_R, fI43_R, fI44_R,  # Secondary recovery
+              fmuS,
+              fmuI1, fmuI2, fmuI3, fmuI4,
+              fmuR1, fmuR2, fmuR3, fmuR4,
+              fmuI11, fmuI12, fmuI13, fmuI14,
+              fmuI21, fmuI22, fmuI23, fmuI24,
+              fmuI31, fmuI32, fmuI33, fmuI34,
+              fmuI41, fmuI42, fmuI43, fmuI44,
+              fmuR]
+
+assert len(propensity) == tm.shape[1]
 
 M = Model(vnames=vnames, rates=pars, inits=ini, tmat=tm, propensity=propensity)
 t0 = time.time()
-M.run(tmax=100, reps=1, viz=0, serial=0)
+M.run(tmax=500, reps=1, viz=0, serial=1)
 print 'total time: {} seconds'.format(time.time() - t0)
 t, series, steps, evts = M.getStats()
 ser = series.mean(axis=0)
 # print evts
-P.plot(t, ser)
-P.legend(M.vn, loc=0)
+P.plot(t, ser[:, 1:])
+P.legend(M.vn[1:], loc=0)
 # P.plot(t, ser[:, 0::3], 'r-.')  # S plots
 # P.plot(t, ser[:, 1::3], 'g-^')  # I plots
 # P.plot(t, ser[:, 2::3], 'b-o')  # R plots
--- Modelo Estocastico/propfun.py
+++ Modelo Estocastico/propfun.py
@@ -1,9 +1,12 @@
-pars = (0.014285714285714285, 400, 1, 100, 1)
-ini = [960, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-N = 1000
+pars = (0.014285714285714285, 400, 1, 10, 1.7)
+ini = [5000, 150, 150, 150, 150, 150, 150, 150, 150, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+N = 5000
 
 lamb = lambda i: 400*(ini[i+1] + sum(ini[9+i : 9+12+i+1 : 4])-ini[9+4*i+1])
-def fS(r, ini): return ini[0]*sum([lamb(i) for i in range(4)])
+
+#Natality
+
+def fnat(r, ini): return r[0]*sum(ini)
 
 #Infection rates
 
@@ -14,48 +17,48 @@ def fS_I4(r, ini): return ini[0]* lamb(3)
 
 #Primary recovery rates
 
-def fI1_R1(r, ini): return r[-2]*ini[5]
-def fI2_R2(r, ini): return r[-2]*ini[6]
-def fI3_R3(r, ini): return r[-2]*ini[7]
-def fI4_R4(r, ini): return r[-2]*ini[8]
+def fI1_R1(r, ini): return r[3]*ini[1]
+def fI2_R2(r, ini): return r[3]*ini[2]
+def fI3_R3(r, ini): return r[3]*ini[3]
+def fI4_R4(r, ini): return r[3]*ini[4]
 
 #Secondary Infection rates
 
-def fR1_I11(r, ini): return r[-1]*ini[5]*lamb(0)
-def fR1_I12(r, ini): return r[-1]*ini[5]*lamb(1)
-def fR1_I13(r, ini): return r[-1]*ini[5]*lamb(2)
-def fR1_I14(r, ini): return r[-1]*ini[5]*lamb(3)
-def fR2_I21(r, ini): return r[-1]*ini[6]*lamb(0)
-def fR2_I22(r, ini): return r[-1]*ini[6]*lamb(1)
-def fR2_I23(r, ini): return r[-1]*ini[6]*lamb(2)
-def fR2_I24(r, ini): return r[-1]*ini[6]*lamb(3)
-def fR3_I31(r, ini): return r[-1]*ini[7]*lamb(0)
-def fR3_I32(r, ini): return r[-1]*ini[7]*lamb(1)
-def fR3_I33(r, ini): return r[-1]*ini[7]*lamb(2)
-def fR3_I34(r, ini): return r[-1]*ini[7]*lamb(3)
-def fR4_I41(r, ini): return r[-1]*ini[8]*lamb(0)
-def fR4_I42(r, ini): return r[-1]*ini[8]*lamb(1)
-def fR4_I43(r, ini): return r[-1]*ini[8]*lamb(2)
-def fR4_I44(r, ini): return r[-1]*ini[8]*lamb(3)
+def fR1_I11(r, ini): return r[4]*ini[5]*lamb(0)
+def fR1_I12(r, ini): return r[4]*ini[5]*lamb(1)
+def fR1_I13(r, ini): return r[4]*ini[5]*lamb(2)
+def fR1_I14(r, ini): return r[4]*ini[5]*lamb(3)
+def fR2_I21(r, ini): return r[4]*ini[6]*lamb(0)
+def fR2_I22(r, ini): return r[4]*ini[6]*lamb(1)
+def fR2_I23(r, ini): return r[4]*ini[6]*lamb(2)
+def fR2_I24(r, ini): return r[4]*ini[6]*lamb(3)
+def fR3_I31(r, ini): return r[4]*ini[7]*lamb(0)
+def fR3_I32(r, ini): return r[4]*ini[7]*lamb(1)
+def fR3_I33(r, ini): return r[4]*ini[7]*lamb(2)
+def fR3_I34(r, ini): return r[4]*ini[7]*lamb(3)
+def fR4_I41(r, ini): return r[4]*ini[8]*lamb(0)
+def fR4_I42(r, ini): return r[4]*ini[8]*lamb(1)
+def fR4_I43(r, ini): return r[4]*ini[8]*lamb(2)
+def fR4_I44(r, ini): return r[4]*ini[8]*lamb(3)
 
 #Secondary Recovery rates
 
-def fI11_R(r, ini): return r[-2]*ini[9]
-def fI12_R(r, ini): return r[-2]*ini[10]
-def fI13_R(r, ini): return r[-2]*ini[11]
-def fI14_R(r, ini): return r[-2]*ini[12]
-def fI21_R(r, ini): return r[-2]*ini[13]
-def fI22_R(r, ini): return r[-2]*ini[14]
-def fI23_R(r, ini): return r[-2]*ini[15]
-def fI24_R(r, ini): return r[-2]*ini[16]
-def fI31_R(r, ini): return r[-2]*ini[17]
-def fI32_R(r, ini): return r[-2]*ini[18]
-def fI33_R(r, ini): return r[-2]*ini[19]
-def fI34_R(r, ini): return r[-2]*ini[20]
-def fI41_R(r, ini): return r[-2]*ini[21]
-def fI42_R(r, ini): return r[-2]*ini[22]
-def fI43_R(r, ini): return r[-2]*ini[23]
-def fI44_R(r, ini): return r[-2]*ini[24]
+def fI11_R(r, ini): return r[3]*ini[9]
+def fI12_R(r, ini): return r[3]*ini[10]
+def fI13_R(r, ini): return r[3]*ini[11]
+def fI14_R(r, ini): return r[3]*ini[12]
+def fI21_R(r, ini): return r[3]*ini[13]
+def fI22_R(r, ini): return r[3]*ini[14]
+def fI23_R(r, ini): return r[3]*ini[15]
+def fI24_R(r, ini): return r[3]*ini[16]
+def fI31_R(r, ini): return r[3]*ini[17]
+def fI32_R(r, ini): return r[3]*ini[18]
+def fI33_R(r, ini): return r[3]*ini[19]
+def fI34_R(r, ini): return r[3]*ini[20]
+def fI41_R(r, ini): return r[3]*ini[21]
+def fI42_R(r, ini): return r[3]*ini[22]
+def fI43_R(r, ini): return r[3]*ini[23]
+def fI44_R(r, ini): return r[3]*ini[24]
 
 #Mortality rates
 
